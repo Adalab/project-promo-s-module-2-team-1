44c3e528f38c33bd2cbf71b70c4f9b46a12516f8
sabfiamo <sabfiamo@gmail.com>
2023-02-07 19:50:57 +0100

    Add previuw.js

---
+++ src/js/02_previuw.js
@@ -0,0 +1,39 @@
+'use strict'
+// const formFill = document.querySelector('.form');
+// // formFill.addEventListener('keyup',fillPreviuw);
+// formFill.addEventListener('change',fillPreviuw);
+// function fillPreviuw(event)
+// {
+// event.preventDefault(); 
+// //Definimos variables para las clases de la preview
+// // const  namePreviuw = document.querySelector('.Name'); 
+// const  jobPreviuw = document.querySelector('.profileRole');
+//     const  telephonePreviuw = document.querySelector('.telephone');
+//     const  emailPreviuw = document.querySelector('.mail');
+//     const  githubPreviuw = document.querySelector('.github');
+//     const  linkelinPreviuw = document.querySelector('.linkedin');
+    
+//     //Previuw con los valores del formulario
+//     switch(event.target.name){
+//     case 'name':
+//         namePreviuw.innerHTML=event.target.value;
+//         prueba.innerHTML=namePreviuw;
+//         break;
+//     case 'puesto':
+//         jobPreviuw.innerHTML=event.target.value;
+//         break;
+//     case 'email':
+//         emailPreviuw.href=event.target.value;
+//         break;
+//     case 'telephone':
+//         telephonePreviuw.href=event.target.value;
+//         break;
+//     case 'github':
+//         githubPreviuw.href=event.target.value;
+//         break;
+//     case 'linkelin':
+//         linkelinPreviuw.href=event.target.value;
+//         break;
+//     }
+   
+// }
