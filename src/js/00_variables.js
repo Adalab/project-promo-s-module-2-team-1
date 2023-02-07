673128189853245516ec2c3f95c03136dc83413f
NataliaPuertac <nataliapuertac@gmail.com>
2023-02-07 12:37:50 +0100

    add collapsables

--- src/js/00_variables.js
+++ src/js/00_variables.js
@@ -1,7 +1,7 @@
 'use strict';
-// const desingLegend = document.querySelector('.js-design-legend');
-// const fillLegend = document.querySelector('.js-fill-legend');
-// const shareLegend = document.querySelector('.js-share-legend');
-// const desingContent = document.querySelector('.js-design-content');
-// const fillContent = document.querySelector('.js-fill-content');
-// const shareContent = document.querySelector('.js-share-content');
+const desingLegend = document.querySelector('.js-design-legend');
+const fillLegend = document.querySelector('.js-fill-legend');
+const shareLegend = document.querySelector('.js-share-legend');
+const desingContent = document.querySelector('.js-design-content');
+const fillContent = document.querySelector('.js-fill-content');
+const shareContent = document.querySelector('.js-share-content');
