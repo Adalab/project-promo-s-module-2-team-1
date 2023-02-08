8df22e183253996abfb2bc21415b1f7ae68d636c
sabfiamo <sabfiamo@gmail.com>
2023-02-08 22:25:48 +0100

    Add mailto and tel to previuw

--- src/js/02_previuw.js
+++ src/js/02_previuw.js
@@ -1,41 +1,41 @@
 'use strict'
 const formFill = document.querySelector('.js-fill-content');
-
-
-formFill.addEventListener('keyup',fillPreviuw);
-// formFill.addEventListener('change',fillPreviuw);
-     
-function fillPreviuw(event)
-{
-event.preventDefault(); 
-//Definimos variables para las clases de la preview
 const  namePreviuw = document.querySelector('.js-card-nameSurname');
 const  jobPreviuw = document.querySelector('.js-card-profileRole');
 const  telephonePreviuw = document.querySelector('.js-card-telephone');
 const  emailPreviuw = document.querySelector('.js-card-mail');
 const  githubPreviuw = document.querySelector('.js-card-github');
 const  linkelinPreviuw = document.querySelector('.js-card-linkedin');
+
+formFill.addEventListener('keyup',fillPreviuw);
+     
+function fillPreviuw(event)
+{
+event.preventDefault(); 
+
+
 //Previuw con los valores del formulario
 switch(event.target.name){
   case 'name':
     console.log(event.target.value);
 
     if (event.target.value==="")     
-       { namePreviuw.innerHTML= "Nombre Apellido";}
+    { namePreviuw.innerHTML= "Nombre Apellido";}
     else
-        {namePreviuw.innerHTML=event.target.value;}
+    {namePreviuw.innerHTML=event.target.value;}
     break;
   case 'puesto':
     if (event.target.value==="")     
-       { jobPreviuw.innerHTML= "Front-end developer";}
+    { jobPreviuw.innerHTML= "Front-end developer";}
     else
     {jobPreviuw.innerHTML=event.target.value;}
     break;
   case 'email':
-    emailPreviuw.href=event.target.value;
+    emailPreviuw.href="mailto:" + event.target.value;
+    console.log(emailPreviuw.href);
     break;
   case 'telephone':
-    telephonePreviuw.href=event.target.value;
+    telephonePreviuw.href="tel:"+ event.target.value;
     break;
   case 'github':
     githubPreviuw.href=event.target.value;
