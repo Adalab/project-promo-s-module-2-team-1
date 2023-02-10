e9ab3adca10c5b4c43f2e29dc1e9467027a776b1
Irene Mezquita <irenemezyar@gmail.com>
2023-02-10 12:58:45 +0100

    add pallettes

--- src/js/03_palette.js
+++ src/js/03_palette.js
@@ -3,7 +3,7 @@
 const color1Input = document.querySelector('.js-color1');
 const color2Input = document.querySelector('.js-color2');
 const color3Input = document.querySelector('.js-color3');
-const namePreview = document.querySelector('js-card-nameSurname');
+const namePreview = document.querySelector('.js-card-nameSurname');
 const telephonePreview = document.querySelector('.js-card-telephone');
 const emailPreview = document.querySelector('.js-card-mail');
 const githubPreview = document.querySelector('.js-card-github');
@@ -13,7 +13,20 @@ const borderPreview = document.querySelector('.js-border');
 const containerColor = document.querySelector('.js-containerColor');
 
 function handleClick(event) {
-  event.preventDefault();
+  // event.preventDefault();
+
+  if (event.target.id==='color1'){
+    namePreview.classList.add('palette1');
+
+  }
+  if (event.target.id==='color2'){
+    namePreview.classList.add('palette2');
+    
+  }
+  if (event.target.id==='color3'){
+    namePreview.classList.add('palette3');
+    
+  }
 
   console.log(event.target.id);
   console.log('hola mundo');
